a6d3e747ad5c95dd59d413ae18df3b439e922c5a
3c220342d2d08c739c0583482c5ef3a6a5894c48 <ishita.roy@in.pwc.com>
2016-12-11 12:09:22 +0530

    commtting the source code
    commtting the source code

---
+++ src/MeSecure/www/js/app.js
@@ -0,0 +1,26 @@
+angular.module('App', ['ionic','ngCordova'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'views/home/home.html',
+      controller: 'HomeController'
+    })
+  $urlRouterProvider.otherwise('/home');
+
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+  
+})
+
